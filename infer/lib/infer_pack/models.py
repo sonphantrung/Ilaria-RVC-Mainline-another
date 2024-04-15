352e0f88d693bb08f8f1881847b8039a5244077e
Son Phan Trung <phantrungson17@gmail.com>
2024-04-15 16:12:54 +0700

    typo fix

--- infer/lib/infer_pack/models.py
+++ infer/lib/infer_pack/models.py
@@ -9,7 +9,7 @@ import torch
 from torch import nn
 from torch.nn import AvgPool1d, Conv1d, Conv2d, ConvTranspose1d
 from torch.nn import functional as F
-from torch.nn.utils import remove_weight_norm, spectral_norm, weight_norm
+from torch.nn.utils import remove_weight_norm, spectral_norm,re weight_norm
 
 from infer.lib.infer_pack import attentions, commons, modules
 from infer.lib.infer_pack.commons import get_padding, init_weights
@@ -174,7 +174,7 @@ class ResidualCouplingBlock(nn.Module):
 
     def __prepare_scriptable__(self):
         for i in range(self.n_flows):
-            if isinstance(self.flows[i * 2]):
+            if isinstance(self.flows[i * 2], WeightNorm):
                 torch.nn.utils.remove_weight_norm(self.flows[i * 2])
 
         return self
